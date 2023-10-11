d2bd0f07c992e54ff329beaec446487a430ff886
PiotrOszczepalski <144422057+PiotrOszczepalski@users.noreply.github.com>
2023-10-11 14:49:03 +0200

    Add files via upload

---
+++ spec03_ochrona_dostepu/app/credit_calc.php
@@ -0,0 +1,101 @@
+<?php
+    require_once dirname(__FILE__).'/../config.php';
+    include _ROOT_PATH.'/app/security/check.php';
+
+    function getParams (&$kwota, &$oprocentowanie, &$okres) {
+
+        $kwota = isset($_REQUEST['kwota'])?$_REQUEST['kwota']:null;
+        $oprocentowanie = isset($_REQUEST['oprocentowanie'])?$_REQUEST['oprocentowanie']:null;
+        $okres = isset($_REQUEST['okres'])?$_REQUEST['okres']:null;
+
+    }
+
+    function validate(&$kwota, &$oprocentowanie, &$okres, &$messages) {
+
+        if (!(isset($kwota) && isset($oprocentowanie) && isset($okres))) {
+            return false;
+        }
+
+        if ($kwota == "") {
+            $messages[] = "Nie podano kwoty kredytu.";
+        }
+
+        if ($oprocentowanie == "") {
+            $messages[] = "Nie podano oprocentowania kredytu.";
+        }
+
+        if ($okres == "") {
+            $messages[] = "Nie podano czasu trwania kredytu.";
+        }
+
+        if (count($messages) != 0) {
+            return false;
+        }
+
+        if (!is_numeric($kwota)) {
+            $messages [] = 'Kwota nie jest liczbą.';
+        }
+        
+        if (!is_numeric($oprocentowanie)) {
+            $messages [] = 'Oprocentowanie nie jest liczbą.';
+        }
+        
+        if (!is_numeric($okres)) {
+            $messages [] = 'Okres nie jest liczbą.';
+        }
+    
+        if (count($messages) != 0) {
+            return false;
+        }
+        else {
+            return true;
+        }
+
+    }
+
+    function process(&$kwota, &$oprocentowanie, &$okres, &$messages, &$rata) {
+
+        global $role;
+    
+        $kwota = intval($kwota);
+        $oprocentowanie = floatval($oprocentowanie);
+        $okres = intval($okres);
+
+        if ($kwota > 10000) {
+            if ($role == 'admin') {
+
+				$procent = (($kwota*$oprocentowanie)/100)*$okres;
+                $kwota2 = $kwota+$procent;
+
+                $miesiac = $okres*12;
+                $rata = $kwota2/$miesiac;
+
+			} 
+            else {
+				$messages [] = 'Tylko administrator może obliczyć ratę kredytu dla kwoty wyższej niż 10 000 zł!';
+			}
+        }
+        else {
+            $procent = (($kwota*$oprocentowanie)/100)*$okres;
+            $kwota2 = $kwota+$procent;
+
+            $miesiac = $okres*12;
+            $rata = $kwota2/$miesiac;
+        }
+
+    }
+
+    $kwota = null;
+    $oprocentowanie = null;
+    $okres = null;
+    $rata = null;
+    $messages = array();
+
+    getParams($kwota, $oprocentowanie, $okres);
+
+    if (validate($kwota, $oprocentowanie, $okres, $messages)) { 
+	    process($kwota, $oprocentowanie, $okres, $messages, $rata);
+    }
+
+    include "credit_calc_view.php";
+?>
---
+++ spec03_ochrona_dostepu/app/credit_calc_view.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="pl">
+<link rel="stylesheet" href="<?php print(_APP_URL);?>/app/style.css">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Kalkulator kredytowy</title>
+</head>
+
+<body>
+	<a href="<?php print(_APP_ROOT); ?>/app/security/logout.php" id="button">Wyloguj</a>
+
+    <form action="<?php print(_APP_URL);?>/app/credit_calc.php" method="post">
+
+        <label for="kwota">Kwota (zł)</label>
+        <input type="text" id="kwota" name="kwota" value="<?php out($kwota) ?>">
+
+        <br>
+
+        <label for="oprocentowanie">Oprocentowanie (%)</label>
+        <input type="text" id="oprocentowanie" name="oprocentowanie" value="<?php out($oprocentowanie) ?>">
+
+        <br>
+
+        <label for="okres">Okres (lata)</label>
+        <input type="text" id="okres" name="okres" value="<?php out($okres) ?>">
+
+        <br>
+
+        <input type="submit" value="Oblicz" id="przycisk">
+
+    </form>
+
+    <?php
+        if (isset($messages)) {
+            if (count($messages) > 0) {
+                
+                echo "<ol id='lista'>";
+
+                foreach ( $messages as $key => $msg ) {
+                    echo "<li>" . $msg . "</li>";
+                }
+
+                echo "</ol>";
+
+            }
+        }
+
+        if (isset($rata)) {
+            echo "<span id='ok'>" . "Miesięczna rata kredytu wyniesie: " . round($rata, 2) . " zł" . "</span>";
+        }
+    ?>
+      
+</body>
+
+</html>
